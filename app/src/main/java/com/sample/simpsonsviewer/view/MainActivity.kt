32b0d6df8b30800a5a4b39672b9681e506b37fd4
joshakins <jakins177@email.com>
2023-06-29 17:15:01 -0500

    added back button functionality

--- app/src/main/java/com/sample/simpsonsviewer/view/MainActivity.kt
+++ app/src/main/java/com/sample/simpsonsviewer/view/MainActivity.kt
@@ -2,12 +2,30 @@ package com.sample.simpsonsviewer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.sample.simpsonsviewer.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+     val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
